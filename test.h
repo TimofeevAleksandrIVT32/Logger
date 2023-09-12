423d0f12073b1865750a7a1fc94b6551563fe924
Timofeev Aleksandr <115435770+TimofeevAleksandrIVT32@users.noreply.github.com>
2023-09-12 04:18:07 +0300

    test.h

---
+++ test.h
@@ -0,0 +1,11 @@
+#ifndef TEST_H
+#define TEST_H
+
+#define STR_SIZE 50
+
+void test_f(char *str);
+void test_fun(char *str);
+void test_funct(char *str);
+void test_function(char *str);
+
+#endif  /* TEST_H */
