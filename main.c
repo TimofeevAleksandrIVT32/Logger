6a8294d686de5d11ce512421fb03b02c4a09e6e4
Timofeev Aleksandr <115435770+TimofeevAleksandrIVT32@users.noreply.github.com>
2023-09-14 21:42:20 +0300

    main.c

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
 #include "test.h"
 #include "log_lib.h"
 
-int main() {
-    log_init();
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Invalid number of arguments\n");
+        return 1;
+    }
+    log_init(argv[1]);
     log_debug("debug message!!!");
     test_f();
     log_close();
