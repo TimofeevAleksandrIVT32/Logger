00fecc022ec821648247f55d1f2af03de4f6d16d
Timofeev Aleksandr <115435770+TimofeevAleksandrIVT32@users.noreply.github.com>
2023-09-14 03:21:13 +0300

    log_lib.c

--- log_lib.c
+++ log_lib.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <stdarg.h>
 #include <execinfo.h>
 #include "log_lib.h"
@@ -8,15 +9,20 @@
 // объявляем макросы здесь, чтобы они не были доступны пользователям библиотеки
 #define BUF_SIZE 100
 #define LEVEL_MAX_SIZE 10
+#define STR_SIZE 50
 
 // объявляем функции и переменную как static, чтобы они не были доступны пользователям библиотеки
 static void select_level(char *l, int level);
+static void get_time();
 static void get_call_stack(int level);
 static FILE *log_file;
 
 // функция инициализации
-void log_init(char *file_name) {
-    log_file = fopen(file_name, "w");
+void log_init() {
+    char file_name[STR_SIZE];
+    printf("Enter the name of the log file\n");
+    scanf("%49s", file_name);
+    log_file = fopen(file_name, "a");
     if (!log_file) {
         perror("Failed to open file");
         exit(EXIT_FAILURE);
@@ -27,7 +33,8 @@ void log_init(char *file_name) {
 void log_message(int level, const char *file, int line, const char* message, ...) {
     char l[LEVEL_MAX_SIZE];
     select_level(l, level);
-    
+    get_time();
+
     fprintf(log_file, " %s:%d: %s: ", file, line, l);
     va_list args;
     va_start(args, message);
@@ -40,6 +47,7 @@ void log_message(int level, const char *file, int line, const char* message, ...
 
 // функция закрытия log файла
 void log_close() {
+    fprintf(log_file, "\n");
     fclose(log_file);
 }
 
@@ -62,6 +70,15 @@ static void select_level(char *l, int level) {
             strcpy(l,"emerg sit");
             break;
     }
+}
+
+// функция получения текущих даты и времени
+static void get_time() {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    char date_time[256];
+    strftime(date_time, sizeof(date_time), "%Y-%m-%d %H:%M:%S", &tm);
+    fprintf(log_file, "[%s]", date_time);
 }
 
 // функция получения стека вызовов
