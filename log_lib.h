ff8793329250712e1959eebaba41df24e983d25f
Timofeev Aleksandr <115435770+TimofeevAleksandrIVT32@users.noreply.github.com>
2023-09-14 03:22:19 +0300

    log_lib.h

--- log_lib.h
+++ log_lib.h
@@ -3,15 +3,21 @@
 
 #include <stdarg.h>
 
-#define log_debug(message, ...) log_message(0, __FILE__, __LINE__, message,  ##__VA_ARGS__)
-#define log_info(message, ...) log_message(1, __FILE__, __LINE__, message, ##__VA_ARGS__)
-#define log_warning(message, ...) log_message(2, __FILE__, __LINE__, message, ##__VA_ARGS__)
-#define log_error(message, ...) log_message(3, __FILE__, __LINE__, message, ##__VA_ARGS__)
+#define LOG_MES_DEBUG 0
+#define LOG_MES_INFO 1
+#define LOG_MES_WARNING 2
+#define LOG_MES_ERROR 3
+#define LOG_MES_EMERG_SIT 4
+
+#define log_debug(...) log_message(LOG_MES_DEBUG, __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(...) log_message(LOG_MES_INFO, __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_warning(...) log_message(LOG_MES_WARNING, __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_error(...) log_message(LOG_MES_ERROR, __FILE__, __LINE__, ##__VA_ARGS__)
 
 // для использования при аварийных ситуациях (например malloc, вернувший NULL)
-#define log_emerg_sit(message, ...) log_message(4, __FILE__, __LINE__, message, ##__VA_ARGS__)
+#define log_emerg_sit(...) log_message(LOG_MES_EMERG_SIT, __FILE__, __LINE__, ##__VA_ARGS__)
 
-void log_init(char *filename);
+void log_init();
 void log_message(int level, const char *file, int line, const char* message, ...);
 void log_close();
 
